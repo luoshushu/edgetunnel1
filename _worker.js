d7c169ca23e31f6c74268741841128502ae2c9b1
Samuel Su <852835916@qq.com>
2025-07-22 14:04:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,10 @@
 
 import { connect } from 'cloudflare:sockets';
 
+
 let userID = '';
 let proxyIP = '';
 let DNS64Server = '';
-//let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
 let subProtocol = 'https';
@@ -14,7 +14,7 @@ let parsedSocks5Address = {};
 let enableSocks = false;
 let enableHttp = false;
 let noTLS = 'false';
-const expire = 4102329600;//2099-12-31
+const expire = 4102329600; //2099-12-31
 let proxyIPs;
 let socks5s;
 let go2Socks5s = [
